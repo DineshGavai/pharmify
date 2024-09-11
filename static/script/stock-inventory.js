fbc227b02f0e18f3e1164fa4fe9a4f56610c207c
Vedant Mali <vedantmali05@gmail.com>
2024-09-11 13:25:59 +0530

    Stock Overview UI complete

--- static/script/stock-inventory.js
+++ static/script/stock-inventory.js
@@ -21,12 +21,16 @@ document.addEventListener("DOMContentLoaded", () => {
             counts: [4, 3, 3]
         },
         financial: {
-            cost: 15000,
-            profitAmount: 3000,
-            lossAmount: 0,
-            sameAsWholesale: 2
+            cost: 1500,
+            profitProductCount: 20,
+            profitPercent: 300,
+            lossProductCount: 20,
+            lossPercent: -20,
+            sameAsWholesaleAmount: 12
         }
     }
+
+    let { financial, count } = overviewData;
 
     // Populate all the last update boxes
     document.querySelectorAll(".date-last-updated")
@@ -40,34 +44,65 @@ document.addEventListener("DOMContentLoaded", () => {
     // Populating Donut Chart
     let donutChartData = [];
 
-    if (overviewData.count.products == 0) {
+    if (count.products == 0) {
         donutChartData = [{ label: "Available", value: 0, color: "var(--clr-grey-divider)" }]
     } else {
         donutChartData = [
-            { label: "Available", value: overviewData.count.productsInStock - overviewData.count.productsLowStock, color: "var(--clr-primary-dark)" },
-            { label: "Low in Stock", value: overviewData.count.productsLowStock, color: "var(--clr-primary-light)" },
-            { label: "Out of Stock", value: overviewData.count.productsOutOfStock, color: "var(--clr-grey-divider)" },
+            { label: "Available", value: count.productsInStock - count.productsLowStock, color: "var(--clr-primary-dark)" },
+            { label: "Low in Stock", value: count.productsLowStock, color: "var(--clr-primary-light)" },
+            { label: "Out of Stock", value: count.productsOutOfStock, color: "var(--clr-grey-divider)" },
         ]
     }
     document.getElementById("overview_donut_chart").innerHTML = createChartDonut(donutChartData);
 
     // Populating other stock overview data
-    document.getElementById("overview_product_count").innerHTML = formatINR(overviewData.count.products, false);
-    document.getElementById("overview_product_in_stock_count").innerHTML = formatINR(overviewData.count.productsInStock, false) + " <span class='subtitle'>Available in Stock</span>";
-    document.getElementById("overview_product_low_stock_count").innerHTML = formatINR(overviewData.count.productsLowStock, false) + " <span class='subtitle'>Low in Stock</span>";
-    document.getElementById("overview_product_out_of_stock_count").innerHTML = formatINR(overviewData.count.productsOutOfStock, false) + " <span class='subtitle'>Out of Stock</span>";
+    document.getElementById("overview_product_count").innerHTML = formatINR(count.products, false);
+    document.getElementById("overview_product_in_stock_count").innerHTML = formatINR(count.productsInStock, false) + " <span class='subtitle'>Available in Stock</span>";
+    document.getElementById("overview_product_low_stock_count").innerHTML = formatINR(count.productsLowStock, false) + " <span class='subtitle'>Low in Stock</span>";
+    document.getElementById("overview_product_out_of_stock_count").innerHTML = formatINR(count.productsOutOfStock, false) + " <span class='subtitle'>Out of Stock</span>";
 
 
     /* ///////////////
     Expiry Overview
     /////////////// */
 
-    let overviewExpiredCount = document.getElementById("overview_expired_count");
-    let overviewExpirySoonCount = document.getElementById("overview_expiry_soon_count");
+    let expiredCount = document.getElementById("overview_expired_count");
+    let expirySoonCount = document.getElementById("overview_expiry_soon_count");
 
-    if (overviewData.count.productsExpired > 0) overviewExpiredCount.classList.remove("safe");
-    if (overviewData.count.productsNearExpiry > 0) overviewExpirySoonCount.classList.remove("safe");
+    if (count.productsExpired > 0) expiredCount.classList.remove("safe");
+    if (count.productsNearExpiry > 0) expirySoonCount.classList.remove("safe");
 
-    overviewExpiredCount.innerHTML = formatINR(overviewData.count.productsExpired, false)
-    overviewExpirySoonCount.innerHTML = formatINR(overviewData.count.productsNearExpiry, false)
+    expiredCount.innerHTML = formatINR(count.productsExpired, false);
+    expirySoonCount.innerHTML = formatINR(count.productsNearExpiry, false);
+
+
+    /* ///////////////
+        FINANCIAL OVERVIEW
+    /////////////// */
+
+    let financialTotalValue = document.getElementById("overview_total_value");
+
+    function setFinancialData(elemCount, elemPercent, countData, percentData, label) {
+        if (percentData && percentData > 0) percentData = "+" + percentData;
+        if (!countData) elemCount.classList.add("safe");
+        if (elemCount) elemCount.innerHTML = formatINR(countData, false);
+        if (elemPercent) elemPercent.innerHTML = percentData ? percentData + "%" : label;
+    }
+
+    financialTotalValue.innerHTML = formatINR(financial.cost, false);
+
+    setFinancialData(
+        document.getElementById("overview_loss_products"), document.getElementById("overview_loss_percent"),
+        financial.lossProductCount, financial.lossPercent, "No Loss"
+    )
+
+    setFinancialData(
+        document.getElementById("overview_same_products"), null,
+        financial.sameAsWholesaleAmount, null, null
+    )
+
+    setFinancialData(
+        document.getElementById("overview_profit_products"), document.getElementById("overview_profit_percent"),
+        financial.profitProductCount, financial.profitPercent, "No Profit"
+    )
 })
