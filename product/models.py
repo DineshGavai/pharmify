6be2f131b30266043d91be61a73aa5713322664c
Dinesh Gavai <gavaidinesh26@gmail.com>
2024-09-12 15:10:21 +0530

    SoldItem table

--- product/models.py
+++ product/models.py
@@ -26,5 +26,14 @@ class Product(models.Model):
      
     def __str__(self):
         return self.name
-    
 
+class SoldItem(models.Model):
+    sold_item_id = models.AutoField(primary_key=True)
+    # name of patient 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='sold_items')
+    quantity_sold = models.IntegerField()
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+         return f"{self.quantity_sold} x {self.product.name} sold on {self.sale_date.strftime('%Y-%m-%d')}"
