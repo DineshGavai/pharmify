142d83dae84d209e1ac4ebd6dd716f60ba106486
Dinesh Gavai <gavaidinesh26@gmail.com>
2024-09-13 23:21:14 +0530

    Retrival and Summarizing Overview Data

--- product/models.py
+++ product/models.py
@@ -1,39 +1,60 @@
 from django.db import models
 from user.models import Owner
 from django.utils import timezone
+
+
 # Create your models here.
 class Seller(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=200)
-    phone_number=models.CharField(max_length=10)
-    
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=10)
+
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     brand = models.CharField(max_length=200)
-    product_type = models.CharField(max_length=20)
+    product_type = models.CharField(max_length=200)
     manufacture_date = models.DateField()
     expiry_date = models.DateField()
     available_quantity = models.IntegerField()
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     wholesale_price = models.DecimalField(max_digits=10, decimal_places=2)
-    seller=models.ForeignKey(Seller,on_delete=models.CASCADE)
-    product_added_date=models.DateTimeField(auto_now_add=True)
-     
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    product_added_date = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
 
+
 class SoldItem(models.Model):
     sold_item_id = models.AutoField(primary_key=True)
-    # name of patient 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='sold_items')
+    # name of patient
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="sold_items"
+    )
     quantity_sold = models.IntegerField()
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     sale_date = models.DateTimeField(default=timezone.now)
-    
+
     def __str__(self):
-         return f"{self.quantity_sold} x {self.product.name} sold on {self.sale_date.strftime('%Y-%m-%d')}"
+        return f"{self.quantity_sold} x {self.product.name} sold on {self.sale_date.strftime('%Y-%m-%d')}"
+
+    def deduct_product_quantity(self):
+        self.product.available_quantity -= self.quantity_sold
+
+        if self.product.available_quantity < 0:
+            raise ValueError("Insufficient stock available for this sale.")
+
+        self.product.save()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # If it's a new sale (not an update)
+            self.deduct_product_quantity()
+
+        # Call the original save method to save the SoldItem instance
+        super().save(*args, **kwargs)
--- product/views.py
+++ product/views.py
@@ -1,70 +1,65 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from .models import Product,Seller
+from .models import *
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Sum
-
+from datetime import timedelta
+from django.utils import timezone
 
 
 def stock_new(request):
-    sellers=list(Seller.objects.values_list("name",flat=True))
-    products=list(Product.objects.values_list("name",flat=True))
-    brands=set(Product.objects.values_list("brand",flat=True))
+    sellers = list(Seller.objects.values_list("name", flat=True))
+    products = list(Product.objects.values_list("name", flat=True))
+    brands = set(Product.objects.values_list("brand", flat=True))
     sellers.reverse()
     context = {
         "currentPage": "stock-new",
-        "sellers":sellers,
-        "products":products,
-        "brands":brands
+        "sellers": sellers,
+        "products": products,
+        "brands": brands,
     }
-    return render(request,'stock/new.html',context)
-
+    return render(request, "stock/new.html", context)
 
 
 @csrf_exempt
 def add_seller(request):
-    if request.method=="POST":
-        seller_name=request.POST.get('new_seller_name')
-        seller_number=request.POST.get('new_seller_phone')
+    if request.method == "POST":
+        seller_name = request.POST.get("new_seller_name")
+        seller_number = request.POST.get("new_seller_phone")
         exists_seller = list(Seller.objects.values_list("name", flat=True))
-        if(seller_name  in exists_seller):
+        if seller_name in exists_seller:
             return HttpResponse("Seller Already Present")
         else:
-            seller=Seller.objects.create(
-                name=seller_name,
-                phone_number=seller_number
-            )
+            seller = Seller.objects.create(name=seller_name, phone_number=seller_number)
             return HttpResponse("Seller Added Successfully")
-
-
 
 
 @csrf_exempt
 def add_stock_summary(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data_str = request.body.decode("utf-8")
 
         try:
             product_data = json.loads(data_str)
-            
-            for product_group in product_data['newProductJSON']:
+
+            for product_group in product_data["newProductJSON"]:
                 for product in product_group:
                     print(product)
-                    name = product.get('name')
-                    expiry_date = product.get('dateExpiry')
-                    brand = product.get('brand')
-                    product_type=product.get('type')
-                    manufcature_date = product.get('dateManufacture')
-                    isNew = product.get('isNew')
-                    selling_price = product.get('priceSelling')
-                    wholesale_price = product.get('priceWholesale')
-                    quantity = product.get('quantity')
-                    seller_name = product.get('sellerName')
-                    seller=Seller.objects.get(name=seller_name)
-                    owner=request.user
-                       
+                    name = product.get("name")
+                    expiry_date = product.get("dateExpiry")
+                    brand = product.get("brand")
+                    product_type = product.get("type")
+                    manufcature_date = product.get("dateManufacture")
+                    isNew = product.get("isNew")
+                    selling_price = product.get("priceSelling")
+                    wholesale_price = product.get("priceWholesale")
+                    quantity = product.get("quantity")
+                    seller_name = product.get("sellerName")
+                    seller = Seller.objects.get(name=seller_name)
+                    owner = request.user
+
                     product = Product.objects.create(
                         owner_id=owner,
                         name=name,
@@ -75,26 +70,28 @@ def add_stock_summary(request):
                         available_quantity=quantity,
                         selling_price=selling_price,
                         wholesale_price=wholesale_price,
-                        seller=seller
+                        seller=seller,
                     )
-                
+
             return JsonResponse({"redirect_url": "/stock/inventory"})
         except json.JSONDecodeError as e:
             return HttpResponse(f"Invalid JSON data: {e}")
 
-    context = {
-        "currentPage": "stock-new"
-    }
-    return render(request,"stock/summary.html",context)
+    context = {"currentPage": "stock-new"}
+    return render(request, "stock/summary.html", context)
 
 
 def stock_inventory(request):
-    products=Product.objects.all()
-    product_quantities = Product.objects.values('name').annotate(total_quantity=Sum('available_quantity'))
-    product_quantity_dict = {item['name']: item['total_quantity'] for item in product_quantities}
-
-    inventory_data={
-        "inventoryData":{
+    products = Product.objects.all()
+    product_quantities = Product.objects.values("name").annotate(
+        total_quantity=Sum("available_quantity")
+    )
+    product_quantity_dict = {
+        item["name"]: item["total_quantity"] for item in product_quantities
+    }
+    # inventory data
+    inventory_data = {
+        "inventoryData": {
             "name": [product.name for product in products],
             "brand": [product.brand for product in products],
             "type": [product.product_type for product in products],
@@ -105,14 +102,54 @@ def stock_inventory(request):
             "priceSelling": [product.selling_price for product in products],
             "Quantity Available": [product.available_quantity for product in products],
         }
-            
     }
+
+    # inventory json overview
+    last_added_product = Product.objects.latest("product_added_date")
+    last_updated = last_added_product.product_added_date
+    products_in_stock = Product.objects.filter(available_quantity__gt=0).order_by(
+        "-product_added_date"
+    )
+    product_in_stock_count = products_in_stock.count()
+    products_out_off_stock = Product.objects.filter(available_quantity__lt=1).count()
+    products_low_in_stock = Product.objects.filter(available_quantity__lte=5).count()
+
+    N = 7  # Number of days before expiry
+    expired_products = Product.objects.filter(
+        expiry_date__lt=timezone.now().date()
+    ).count()
+
+    near_expiry_date = timezone.now().date() + timedelta(days=N)
+    products_near_expiry = Product.objects.filter(
+        expiry_date__range=(timezone.now().date(), near_expiry_date),
+        available_quantity__gt=0,
+    ).count()
+
+    # most sold products
+    most_sold = SoldItem.objects.filter(quantity_sold__gt=20).count()
+    least_sold = SoldItem.objects.filter(quantity_sold__lt=5).count()
+
+    inventory_overview = {
+        "lastUpdated": last_updated,
+        "count": {
+            "products": products.count(),
+            "productsInStock": product_in_stock_count,
+            "productsOutOfStock": products_out_off_stock,
+            "productsLowStock": products_low_in_stock,
+            "productExpired": expired_products,
+            "productsNearExpiry": products_near_expiry,
+            "productsMostSold": most_sold,
+            "productsLeastSold": least_sold,
+        },
+        "types": {
+            "names": [product.name for product in products_in_stock],
+            "counts": [
+                "Array of products available in corresponding types (Descending order)"
+            ],
+        },
+    }
+
     json_data = json.dumps(inventory_data, cls=DjangoJSONEncoder)
 
-    
-    context = {
-        "currentPage": "stock-inventory",
-        "inventory_data":json_data  
-        
-    }
+    context = {"currentPage": "stock-inventory", "inventory_data": json_data}
     return render(request, "stock/inventory.html", context)
