3acbe9c9918b1fd080960b50bba2c34f1d309aba
Dinesh Gavai <gavaidinesh26@gmail.com>
2024-09-14 23:54:38 +0530

    overview complete

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product,Seller
+from .models import *
+
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Seller)
+admin.site.register(SoldItem)
